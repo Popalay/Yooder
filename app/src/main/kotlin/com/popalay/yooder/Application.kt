0abe859c230b7170991acda3b1780406339133af
Denis Nikiforov <popalay@ex.ua>
2015-12-02 21:55:06 +0200

    Реализовал модель "Долг"

--- app/src/main/kotlin/com/popalay/yooder/Application.kt
+++ app/src/main/kotlin/com/popalay/yooder/Application.kt
@@ -2,6 +2,8 @@ package com.popalay.yooder
 
 import android.app.Application
 import com.parse.Parse
+import com.parse.ParseObject
+import com.popalay.yooder.model.Debt
 
 public class Application : Application() {
 
@@ -9,6 +11,7 @@ public class Application : Application() {
         super.onCreate()
 
         Parse.enableLocalDatastore(this)
+        ParseObject.registerSubclass(Debt::class.java)
         Parse.initialize(this, "xIjz6RqkS2yZjhhRWKT2jo0zW0tnVXYiDuDPmHBX", "roIv6s0Tf1dTwyTaHvJd5RMsqBh9xTzLjgA7DiJH")
     }
 }
---
+++ app/src/main/kotlin/com/popalay/yooder/model/Debt.kt
@@ -0,0 +1,35 @@
+package com.popalay.yooder.model
+
+import com.parse.ParseClassName
+import com.parse.ParseObject
+import com.parse.ParseUser
+import java.util.*
+
+enum class Currency(val currency: String) {
+    UAH("₴"), RUB("руб"), EUR("€"), USD("$"), GBP("£")
+}
+
+@ParseClassName("Debt")
+class Debt() : ParseObject() {
+    var author: ParseUser
+        get() = getParseUser("author")
+        set(value) = put("author", value)
+    var title: String
+        get() = getString("title")
+        set(value) = put("title", value)
+    var debtor: ParseUser
+        get() = getParseUser("debtor")
+        set(value) = put("debtor", value)
+    var debtorStr: String
+        get() = getString("debtorStr")
+        set(value) = put("debtorStr", value)
+    var amount: Double
+        get() = getDouble("amount")
+        set(value) = put("amount", value)
+    var currency: Currency
+        get() = Currency.valueOf(getString("currency"))
+        set(value) = put("amount", value.toString())
+    var date: Date
+        get() = getDate("date")
+        set(value) = put("date", value)
+}

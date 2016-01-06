612c8675db95297bb65352924ea039aecbb4dc15
Denis Nikiforov <popalay1@gmail.com>
2016-01-06 13:08:11 +0200

    Added caching

--- app/src/main/kotlin/com/popalay/yooder/Application.kt
+++ app/src/main/kotlin/com/popalay/yooder/Application.kt
@@ -10,8 +10,9 @@ public class Application : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        //Parse.enableLocalDatastore(this)
+        Parse.enableLocalDatastore(this)
         ParseObject.registerSubclass(Debt::class.java)
         Parse.initialize(this, "xIjz6RqkS2yZjhhRWKT2jo0zW0tnVXYiDuDPmHBX", "roIv6s0Tf1dTwyTaHvJd5RMsqBh9xTzLjgA7DiJH")
+        Parse.setLogLevel(Parse.LOG_LEVEL_DEBUG)
     }
 }
